a45298cf41cfea68ca0711f371d746956e6eb61d
Embler <vbembler@gmail.com>
2020-08-19 12:56:15 +0300

    Initial Commit

---
+++ TwoViews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwoViews
+//
+//  Created by Vladimir on 19/08/2020.
+//  Copyright © 2020 Embler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
